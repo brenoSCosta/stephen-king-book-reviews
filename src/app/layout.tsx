bce8092582d805cf3c84ce5ba3aeb352f430158c
Breno Souza Costa <brenosouza.dev@gmail.com>
2024-03-15 19:21:57 -0300

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
 import StyledComponentsRegistry from "@/lib/registry";
+import Head from "next/head";
+
+
+
 
 export default function RootLayout({
   children,
@@ -10,9 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+   <Head>
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+      <link rel="shortcut icon" href="../../public/favicon.ico" />
+   </Head>
+     
       <body>
         <StyledComponentsRegistry>
           <>
